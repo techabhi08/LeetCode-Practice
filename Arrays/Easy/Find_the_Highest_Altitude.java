284e8f0dabe04ca44275809e30ed1f2c9986e3ab
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2021-10-04 19:54:47 +0530

    Create Find_the_Highest_Altitude.java

---
+++ Arrays/Easy/Find_the_Highest_Altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int sum = 0;
+        for(int i = 0; i < gain.length; i++){
+            sum += gain[i];
+            if(sum > max)
+                max = sum;
+        }
+        return max;
+    }
+}
