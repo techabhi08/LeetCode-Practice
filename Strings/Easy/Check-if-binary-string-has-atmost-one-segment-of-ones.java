722a5602184813d56aeceecd585071cc614776d5
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-02-05 10:40:59 +0530

    Create Check-if-binary-string-has-atmost-one-segment-of-ones.java

---
+++ Strings/Easy/Check-if-binary-string-has-atmost-one-segment-of-ones.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        String word = "01";
+        if(s.contains(word)){
+            return false;
+        }
+        return true;
+    }
+}
