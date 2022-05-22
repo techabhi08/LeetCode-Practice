786cd8a8501daec4d187e510888e66520129c286
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-05-22 19:39:19 +0530

    Create code.java

---
+++ Arrays/Medium/75. Sort Colors/code.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                red++;
+            }else if(nums[i] == 1){
+                white++; 
+            }else{
+                blue++;
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(i < red){
+                nums[i] = 0;
+            }else if(i < red + white){
+                nums[i] = 1;
+            }else{
+                nums[i] = 2;
+            }
+        }
+    }
+}
