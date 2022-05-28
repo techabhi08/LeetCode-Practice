d4eff84326ea5f021ebdf6af7770fcd0517c293c
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-05-28 14:11:55 +0530

    Create code.java

---
+++ Sorting/Easy/88. Merge Sorted Array/code.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int a = m - 1;
+        int b = n - 1;
+        int c = m + n - 1;
+        while(a >= 0 && b >= 0){
+            if(nums1[a] > nums2[b]){
+                nums1[c] = nums1[a];
+                a--;
+                c--;
+            }else{
+                nums1[c] = nums2[b];
+                b--;
+                c--;
+            }
+        }
+        
+        while(b >= 0){
+            nums1[c] = nums2[b];
+            b--;
+            c--;
+        }
+    }
+}
