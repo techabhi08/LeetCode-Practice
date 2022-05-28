ab3aac824a6957f3d68b44b394896ec50edae8f3
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-05-28 14:15:26 +0530

    Update and rename Sorting/Easy/Find All Numbers Disappeared in an Array.java to Sorting/Easy/448. Find All Numbers Disappeared in an Array/code.java

--- Sorting/Easy/448. Find All Numbers Disappeared in an Array/code.java
+++ Sorting/Easy/448. Find All Numbers Disappeared in an Array/code.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> ans = new ArrayList<Integer>();
+        List<Integer> ans = new ArrayList<>();
         int i = 0;
         while(i < nums.length){
             int correct = nums[i] - 1;
             if(nums[i] != nums[correct]){
-                swap(nums, i, correct);
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
             }else{
                 i++;
             }
         }
         
-        for(int j = 0; j < nums.length; j++) {
+        int j = 0;
+        while(j < nums.length){
             if(nums[j] != j + 1){
-                ans.add(j  + 1);
+                ans.add(j + 1);
             }
+            j++;
         }
         return ans;
-    }
-    
-    public void swap(int[] nums, int first, int second){
-        int temp = nums[first];
-        nums[first] = nums[second];
-        nums[second] = temp;
     }
 }
