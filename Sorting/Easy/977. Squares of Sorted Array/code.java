7148473df769a28e56baacd481374216dae1cd63
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-06-02 18:57:53 +0530

    Create code.java

---
+++ Sorting/Easy/977. Squares of Sorted Array/code.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+//         int[] ans = new int[nums.length];
+//         for(int i = 0; i < nums.length; i++){
+//             ans[i] = nums[i]* nums[i];
+//         }
+        
+//         Arrays.sort(ans);
+//         return ans;
+        
+        
+        int[] result = new int[nums.length];
+        int start = 0;
+        int end = nums.length - 1;
+        
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(Math.abs (nums[start]) >= Math.abs (nums[end])){
+                result[i] = nums[start] * nums[start];
+                start++;
+            }else{
+                result[i] = nums[end] * nums[end];
+                end--;
+            }
+        }
+        return result;
+    }
+}
