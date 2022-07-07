0012e78c2e4b2519fd207f62c706b01390c6da90
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-07-07 12:27:01 +0530

    Create code.java

---
+++ Bitwise/Easy/832. Flipping an Image/code.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int i = 0; i < image.length; i++){
+            int start = 0;
+            int end = image[i].length - 1;
+            while(start < end){
+                int temp = image[i][start];
+                image[i][start] = image[i][end];
+                image[i][end] = temp;
+                start++;
+                end--;
+            }
+            
+            for(int j = 0; j < image.length; j++){
+                // if(image[i][j] == 0){
+                //     image[i][j] = 1;
+                // }else{
+                //     image[i][j] = 0;
+                // }
+                image[i][j] = image[i][j] ^ 1;
+            }
+        }
+        return image;
+    }
+}
