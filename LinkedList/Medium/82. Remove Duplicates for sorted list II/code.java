410e7cc6de5945b9279f9a329e17bd739b5c087f
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-07-22 18:38:52 +0530

    Create code.java

---
+++ LinkedList/Medium/82. Remove Duplicates for sorted list II/code.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode temp = head;
+        ListNode prev = dummy;
+        
+        while(temp != null){
+            if(temp.next != null && temp.val == temp.next.val){
+                while(temp.next != null && temp.val == temp.next.val){
+                    temp = temp.next;
+                }
+                prev.next = temp.next;
+            }else{
+                prev = temp;
+            }
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
