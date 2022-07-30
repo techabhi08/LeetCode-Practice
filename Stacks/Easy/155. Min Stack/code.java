9b9a9f7a5fa44f2647b0b0505f2d7e9a470bb087
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-07-30 14:16:14 +0530

    Update code.java

--- Stacks/Easy/155. Min Stack/code.java
+++ Stacks/Easy/155. Min Stack/code.java
@@ -1,32 +1,60 @@
 class MinStack {
-    int min = Integer.MAX_VALUE;
     Stack<Integer> stack;
-
+    Stack<Integer> minStack;
+    
     public MinStack() {
         stack = new Stack<>();
+        minStack = new Stack<>();
     }
     
-    public void push(int val) {
-        if(val <= min){
-            stack.push(min);
-            min = val;
+    public void push(int val){
+        if(minStack.empty() || val <= minStack.peek()){
+            minStack.push(val);
         }
         stack.push(val);
     }
     
-    public void pop() {
-        if(stack.pop() == min){
-            min = stack.pop();
+    public void pop(){
+        if(stack.pop().equals(minStack.peek())){
+            minStack.pop();
         }
     }
     
-    public int top() {
+    public int top(){
         return stack.peek();
     }
     
-    public int getMin() {
-        return min;
+    public int getMin(){
+        return minStack.peek();
     }
+//     int min = Integer.MAX_VALUE;
+//     Stack<Integer> stack;
+
+//     public MinStack() {
+//         stack = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         if(val <= min){
+//             stack.push(min);
+//             min = val;
+//         }
+//         stack.push(val);
+//     }
+    
+//     public void pop() {
+//         if(stack.pop() == min){
+//             min = stack.pop();
+//         }
+//     }
+    
+//     public int top() {
+//         return stack.peek();
+//     }
+    
+//     public int getMin() {
+//         return min;
+//     }
 }
 
 /**
