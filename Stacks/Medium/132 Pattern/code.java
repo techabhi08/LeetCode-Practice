ff186f1339bdd3fd551188a4ccb8c0d6bb087743
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-08-03 15:23:11 +0530

    Create code.java

---
+++ Stacks/Medium/132 Pattern/code.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int second = Integer.MIN_VALUE;
+        
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(nums[i] < second){
+                return true;
+            }
+            while(!stack.isEmpty() && stack.peek() < nums[i]){
+                second = stack.pop();
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
