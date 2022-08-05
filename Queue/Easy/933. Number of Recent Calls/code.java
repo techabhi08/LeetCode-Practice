c7799c7700dc9bc38dadb4bb1964f01dd0f6f428
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-08-05 17:44:52 +0530

    Create code.java

---
+++ Queue/Easy/933. Number of Recent Calls/code.java
@@ -0,0 +1,23 @@
+class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        queue.offer(t);
+        
+        while(!queue.isEmpty() && t - queue.peek() > 3000){
+            queue.poll();
+        }
+        
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
