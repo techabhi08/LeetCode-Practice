6a11551b1e72ebb6fd2f58ba74bbed8c56a43a2c
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-09-03 09:32:28 +0530

    Create code.java

---
+++ Searching/Medium/875. Koko Eating Bananas/code.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 1000000000;
+        int k = 0;
+        while(low <= high){
+            k = low + (high - low) / 2;
+            int hours = 0;
+            for(int i = 0; i < piles.length; i++){
+                hours += Math.ceil(1.0 * piles[i] / k);
+            }
+             if(hours > h){
+                low = k + 1;
+            }else{
+                high = k - 1;
+            }
+        }
+        return low;
+    }
+}
