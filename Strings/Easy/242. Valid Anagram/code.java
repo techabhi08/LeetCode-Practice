f685f836c6b9a2fc311b4da9cc09630c2cb6ad72
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-09-29 09:07:38 +0530

    Create code.java

---
+++ Strings/Easy/242. Valid Anagram/code.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        int[] charFreq = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            charFreq[s.charAt(i) - 'a']++;
+            charFreq[t.charAt(i) - 'a']--;
+        }
+        
+        for(int i : charFreq){
+            if(i != 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
