59a8e8e1819bf483e76c282ee34f1b5b5fd70889
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-10-15 11:35:57 +0530

    Create code.java

---
+++ Strings/Medium/1328. Break a Palindrome/code.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        if(palindrome.length() < 2){
+            return "";
+        }
+        char[] arr = palindrome.toCharArray();
+        
+        for(int i = 0; i < arr.length / 2; i++){
+            if(arr[i] != 'a'){
+                arr[i] = 'a';
+                return String.valueOf(arr);
+            }
+        }
+        
+        arr[arr.length - 1] = 'b';
+        return String.valueOf(arr);
+    }
+}
