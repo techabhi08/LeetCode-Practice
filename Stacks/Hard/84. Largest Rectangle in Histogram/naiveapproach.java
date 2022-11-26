12bf806d3df0c2fe4220fdb67efbdd38b9333bd4
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-11-26 18:44:18 +0530

    Create naiveapproach.java

---
+++ Stacks/Hard/84. Largest Rectangle in Histogram/naiveapproach.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int[] leftSmaller = new int[heights.length];
+        int[] rightSmaller = new int[heights.length];
+        
+        //Calculating the left smaller for every bar
+        for(int i = 0; i < heights.length; i++){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                    stack.pop();
+            }
+            if(stack.isEmpty()){
+                leftSmaller[i] = 0;
+            }else{
+                int top = stack.peek();
+                leftSmaller[i] = top + 1;
+            }
+            stack.push(i);
+        }
+        
+        //Clearing the stack to reuse
+        while(!stack.isEmpty()){
+            stack.pop();
+        }
+        //Calculating right smaller for each bar
+        for(int i = heights.length - 1; i >= 0; i--){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                    stack.pop();
+            }
+            if(stack.isEmpty()){
+                stack.push(i);
+                rightSmaller[i] = heights.length - 1;
+            }else{
+                int top = stack.peek();
+                rightSmaller[i] = top - 1;
+            }
+            stack.push(i);
+        }
+        
+        //Calculating max Area
+        int maxArea = 0;
+        for(int i = 0; i < heights.length; i++){
+            int width = rightSmaller[i] - leftSmaller[i] + 1;
+            maxArea = Math.max(maxArea, width * heights[i]);
+        }
+        return maxArea;
+    }
+}
