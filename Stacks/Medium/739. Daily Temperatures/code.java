4df192b28f725ff17bf0d54c926172f414502e72
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-12-18 12:00:19 +0530

    Create code.java

---
+++ Stacks/Medium/739. Daily Temperatures/code.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        
+        for(int i = n - 1; i >= 0; i--){
+            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]){
+                stack.pop();
+            }
+            
+            if(!stack.isEmpty()){
+                ans[i] = stack.peek() - i;
+            }
+            
+            stack.push(i);
+        }
+        return ans;
+    }
+}
