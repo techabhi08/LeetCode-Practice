6e7075aa085a182267916d78ca345b91440c1ed4
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-12-23 11:51:54 +0530

    Create spaceOpt.java

---
+++ Dynamic Programming/Medium/62. Unique Paths/spaceOpt.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] prev = new int[n];
+        for(int i = 0; i < m; i++){
+            int[] temp = new int[n];
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    temp[j] = 1;
+                }else{
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0){
+                        up = prev[j];
+                    }
+                    if(j > 0){
+                        left = temp[j - 1];
+                    }
+                    temp[j] = up + left;
+                }
+            }
+            prev = temp;
+        }
+        return prev[n - 1];
+    }
+}
