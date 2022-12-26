df06700d3cdf52833ef7d46e3a43b936b2172c02
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-12-26 22:48:02 +0530

    Create tabulation.java

---
+++ Dynamic Programming/Medium/120. Triangle/tabulation.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int i = 0; i < n; i++){
+            dp[n - 1][i] = triangle.get(n - 1).get(i);
+        }
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                int down = triangle.get(i).get(j) + dp[i + 1][j];
+                int diag = triangle.get(i).get(j) + dp[i + 1][j + 1];
+                dp[i][j] = Math.min(down, diag);
+            }
+        }
+        return dp[0][0];
+    }
+}
