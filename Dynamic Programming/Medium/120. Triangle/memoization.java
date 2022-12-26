71a2cdb027604e123131b42fa2919079bce9b7da
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-12-26 22:47:24 +0530

    Create memoization.java

---
+++ Dynamic Programming/Medium/120. Triangle/memoization.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return findTotal(0, 0, triangle, dp);
+    }
+    
+    public int findTotal(int row, int col, List<List<Integer>> triangle, int[][] dp){
+        if(row == triangle.size() - 1){
+            return triangle.get(row).get(col);
+        }
+        
+        if(dp[row][col] != -1){
+            return dp[row][col];
+        }
+        
+        int down = triangle.get(row).get(col) + findTotal(row + 1, col, triangle, dp);
+        int diag = triangle.get(row).get(col) + findTotal(row + 1, col + 1, triangle, dp);
+        
+        return dp[row][col] = Math.min(down, diag);
+    }
+}
