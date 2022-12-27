6dac5d1edc7c6e08ff320ceaba66f3498d41eaa2
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-12-27 17:24:39 +0530

    Create code.java

---
+++ Sorting/Medium/137. Single Number II/code.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int ans = -1;
+        for(int i = 0; i < nums.length - 1; i += 3){
+            if(nums[i] != nums[i + 1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
