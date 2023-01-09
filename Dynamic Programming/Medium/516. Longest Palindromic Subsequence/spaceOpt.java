352d4b211467727c020297710bc56b04f9655f0f
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-01-09 18:19:25 +0530

    Create spaceOpt.java

---
+++ Dynamic Programming/Medium/516. Longest Palindromic Subsequence/spaceOpt.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        String s2 = sb.reverse().toString();
+        
+        int[] prev = new int[n + 1];
+        int[] curr = new int[n + 1];
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(s.charAt(i - 1) == s2.charAt(j - 1)){
+                    curr[j] = 1 + prev[j - 1];
+                }else{
+                    curr[j] = Math.max(prev[j], curr[j - 1]);
+                }
+            }
+            prev = (int[])curr.clone();
+        }
+        return prev[n];
+    }
+}
