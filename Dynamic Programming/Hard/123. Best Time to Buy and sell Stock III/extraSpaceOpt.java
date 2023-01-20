7bcd589e02f928d4e4852c2a85698e5f48fbb2e7
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-01-20 12:35:07 +0530

    Create extraSpaceOpt.java

---
+++ Dynamic Programming/Hard/123. Best Time to Buy and sell Stock III/extraSpaceOpt.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] ahead = new int[2][3];
+        int[][] curr = new int[2][3];
+        
+        for(int index = n - 1; index >= 0; index--){
+            for(int count = 1; count <= 2; count++){
+                curr[1][count] = Math.max(-prices[index] + ahead[0][count], ahead[1][count]);
+                curr[0][count] = Math.max(prices[index] + ahead[1][count-1], ahead[0][count]);
+            }
+            ahead = (int[][])curr.clone();
+        }
+        return ahead[1][2];
+    }
+}
