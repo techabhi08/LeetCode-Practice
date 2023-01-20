fe53df03a19415b40349779e82385a6e1aba51b3
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-01-20 12:33:58 +0530

    Create tabulation.java

---
+++ Dynamic Programming/Hard/123. Best Time to Buy and sell Stock III/tabulation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n + 1][2][3];
+        
+        for(int index = n - 1; index >= 0; index--){
+            for(int canBuy = 0; canBuy <= 1; canBuy++){
+                for(int count = 1; count <= 2; count++){
+                    if(canBuy == 1){
+                        dp[index][canBuy][count] = Math.max(-prices[index] + 
+                                    dp[index + 1][0][count], dp[index + 1][1][count]);
+                    }else{
+                        dp[index][canBuy][count] = Math.max(prices[index] + 
+                                    dp[index + 1][1][count - 1], dp[index + 1][0][count]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
+    }
+}
