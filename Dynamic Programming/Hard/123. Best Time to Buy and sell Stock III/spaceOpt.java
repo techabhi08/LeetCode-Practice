d29e9b34da07384c2a545c78f3783f0e87080d08
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-01-20 12:34:27 +0530

    Create spaceOpt.java

---
+++ Dynamic Programming/Hard/123. Best Time to Buy and sell Stock III/spaceOpt.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] ahead = new int[2][3];
+        int[][] curr = new int[2][3];
+        
+        for(int index = n - 1; index >= 0; index--){
+            for(int canBuy = 0; canBuy <= 1; canBuy++){
+                for(int count = 1; count <= 2; count++){
+                    if(canBuy == 1){
+                        curr[canBuy][count] = Math.max(-prices[index] + 
+                                    ahead[0][count], ahead[1][count]);
+                    }else{
+                        curr[canBuy][count] = Math.max(prices[index] + 
+                                    ahead[1][count - 1], ahead[0][count]);
+                    }
+                }
+            }
+            ahead = (int[][])curr.clone();
+        }
+        return ahead[1][2];
+    }
+}
