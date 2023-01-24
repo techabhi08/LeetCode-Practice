f57e511456363f71cef57179c63041b374e0500d
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-01-24 10:52:39 +0530

    Create spaceOpt.java

---
+++ Dynamic Programming/Medium/309. Best Time to Buy and Sell Stock With Cooldown/spaceOpt.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2];
+        
+        for(int index = n - 1; index >= 0; index--){
+            dp[index][1] = Math.max(-prices[index] + dp[index + 1][0],dp[index + 1][1]);
+            dp[index][0] = Math.max(prices[index] + 
+                            ((index + 2) <= n ? dp[index + 2][1] : 0), dp[index + 1][0]);
+        }
+        return dp[0][1];
+    }
+}
