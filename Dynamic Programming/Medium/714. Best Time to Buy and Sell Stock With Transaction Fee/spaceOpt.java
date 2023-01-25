20725dc356f65c16c4c092329d957d0bbd04bdc0
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-01-25 10:47:17 +0530

    Create spaceOpt.java

---
+++ Dynamic Programming/Medium/714. Best Time to Buy and Sell Stock With Transaction Fee/spaceOpt.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[] ahead = new int[2];
+        int[] curr = new int[2];
+        
+        for(int index = n - 1; index >= 0; index--){
+            curr[1] = Math.max(-prices[index] + ahead[0], ahead[1]);
+            curr[0] = Math.max(prices[index] - fee+ ahead[1], ahead[0]);
+            
+            ahead = (int[]) curr.clone();
+        }
+        return ahead[1];
+    }
+}
