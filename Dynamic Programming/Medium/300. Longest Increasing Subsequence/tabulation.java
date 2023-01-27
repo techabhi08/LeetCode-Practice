8717861c7e62de3e4c84d424cc3bc5683cf8caea
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-01-27 10:32:00 +0530

    Create tabulation.java

---
+++ Dynamic Programming/Medium/300. Longest Increasing Subsequence/tabulation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][n + 1];
+        
+        for(int index = n - 1; index >= 0; index--){
+            for(int prev = index - 1; prev >= -1; prev--){
+                int take = 0;
+                int notTake = dp[index + 1][prev + 1];
+                if(prev == -1 || nums[prev] < nums[index]){
+                    take = 1 + dp[index + 1][index + 1];
+                }
+                dp[index][prev + 1] = Math.max(take, notTake);
+            }
+        }
+        return dp[0][0];
+    }
+}
