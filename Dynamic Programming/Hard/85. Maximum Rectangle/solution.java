036568ae72ed830140598b993b12c8507a0e2760
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-02-04 11:22:27 +0530

    Create solution.java

---
+++ Dynamic Programming/Hard/85. Maximum Rectangle/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] heights = new int[m];
+        
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == '0'){
+                    heights[j] = 0;
+                }else{
+                    heights[j]++;
+                }
+            }
+            max = Math.max(max, findArea(heights));
+        }
+        return max;
+    }
+    
+    public int findArea(int[] heights){
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        for(int i = 0; i <= heights.length; i++){
+            while(!stack.isEmpty() && (i == heights.length || heights[stack.peek()] >= heights[i])){
+                int height = heights[stack.peek()];
+                stack.pop();
+                int width;
+                if(stack.isEmpty()){
+                    width = i;
+                }else{
+                    width = i - stack.peek() - 1;
+                }
+                maxArea = Math.max(maxArea, width * height);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
