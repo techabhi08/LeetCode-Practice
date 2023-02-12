da3bd6675873ab1d3d02c2cc634cfad32a6ea2b7
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-02-12 19:04:04 +0530

    Create code.java

---
+++ Hashing/Medium/451. Sort Characters By Frequency/code.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        List<Character> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (a, b) -> map.get(b) - map.get(a));
+        
+        StringBuilder sb = new StringBuilder();
+        for(char c : list){
+            for(int i = 0; i < map.get(c); i++){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
