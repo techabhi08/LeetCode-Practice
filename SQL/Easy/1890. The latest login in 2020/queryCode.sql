0d813945af0cc67135facc32a72f1b56e77439a1
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-04-11 19:50:03 +0530

    Create queryCode.sql

---
+++ SQL/Easy/1890. The latest login in 2020/queryCode.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT DISTINCT(user_id), FIRST_VALUE(time_stamp) 
+OVER(PARTITION BY user_id ORDER BY time_stamp DESC) as last_stamp 
+FROM Logins
+WHERE YEAR(time_stamp) = '2020';
