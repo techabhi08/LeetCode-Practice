beb13b0c7032bf735eb28a771e1292b23134b2b0
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-06 22:14:11 +0530

    Time: 16 ms (40.87%), Space: 44.9 MB (82.40%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] == 0){
+                    vis[i][j] = true;
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+        
+        int steps = 0;
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            steps++;
+            
+            for(int i = 0; i < size; i++){
+                int[] curr = queue.poll();
+                int row = curr[0];
+                int col = curr[1];
+                
+                for(int[] dir : dirs){
+                    int nRow = row + dir[0];
+                    int nCol = col + dir[1];
+                    
+                    if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && !vis[nRow][nCol]){
+                        vis[nRow][nCol] = true;
+                        queue.add(new int[]{nRow, nCol});
+                        mat[nRow][nCol] = steps;
+                    }
+                }
+            }
+        }
+        return mat;
+    }
+}
