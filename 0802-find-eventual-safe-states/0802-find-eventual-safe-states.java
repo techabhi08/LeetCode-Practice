c03c4ce8337d9a63d28d7c8a131fd54068bbb8bf
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-17 20:01:50 +0530

    Time: 31 ms (31.06%), Space: 52.9 MB (5.45%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,28 +1,26 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<Integer> ans = new ArrayList<>();
-        int n = graph.length;
-        
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < graph.length; i++){
             adj.add(new ArrayList<>());
         }
         
-        int[] indegree = new int[n];
-        for(int i = 0; i < n; i++){
+        int[] indegree = new int[graph.length];
+        for(int i = 0; i < graph.length; i++){
             for(int item : graph[i]){
-                indegree[i]++;
                 adj.get(item).add(i);
+                indegree[i]++;
             }
         }
         
         Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < indegree.length; i++){
             if(indegree[i] == 0){
                 queue.add(i);
             }
         }
         
+        List<Integer> ans = new ArrayList<>();
         while(!queue.isEmpty()){
             int curr = queue.poll();
             ans.add(curr);
