f9ec2a1994821c7e7f5d8ada28632d3d8eb8b05e
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-21 10:23:34 +0530

    Time: 86 ms (28.67%), Space: 46.6 MB (57.74%) - LeetHub

---
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -0,0 +1,26 @@
+/**
+ * @param {any} o1
+ * @param {any} o2
+ * @return {boolean}
+ */
+var areDeeplyEqual = function(o1, o2) {
+    var objs = [[o1, o2]];
+    while (objs.length) {
+        [o1, o2] = objs.pop();
+
+        if (o1 === o2) continue;
+        if (typeof o1 !== 'object' || typeof o2 !== 'object') return false;
+        if (Array.isArray(o1) !== Array.isArray(o2)) return false;
+
+        const keys1 = Object.keys(o1);
+        const keys2 = Object.keys(o2);
+
+        if (keys1.length !== keys2.length) return false;
+        for (const key of keys1) {
+          if (!(key in o2)) return false;
+          objs.push([o1[key], o2[key]]);
+        }
+  }
+
+  return true;
+}
