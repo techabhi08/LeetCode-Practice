55b94fac6acd07c5a6edfbbf50b085612b416f2f
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-30 17:51:50 +0530

    Time: 2 ms (84.24%), Space: 41.7 MB (89.97%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,6 +3,7 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int fresh = 0;
+        
         Queue<int[]> queue = new LinkedList<>();
         
         for(int i = 0; i < n; i++){
@@ -16,8 +17,7 @@ class Solution {
         }
         
         int mins = 0;
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         while(!queue.isEmpty() && fresh != 0){
             int size = queue.size();
             mins++;
@@ -39,6 +39,7 @@ class Solution {
                 }
             }
         }
+        
         return fresh == 0 ? mins : -1;
     }
 }
