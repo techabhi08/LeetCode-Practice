4c94d1abb53bb1cdcb5dbee6e45a5b7f2ede37ca
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-30 18:32:46 +0530

    Time: 1 ms (43.60%), Space: 44.1 MB (10.03%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,21 +1,33 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length;
+        int m = image[0].length;
         int org = image[sr][sc];
         
         if(org == color){
             return image;
         }
-        dfs(image, sr, sc, color, org);
-        return image;
-    }
-    
-    public void dfs(int[][] image, int sr, int sc, int color, int src){
-        if(sr >= 0 && sr < image.length && sc >= 0 && sc < image[0].length && image[sr][sc] == src){
-            image[sr][sc] = color;
-            dfs(image, sr + 1, sc, color, src);
-            dfs(image, sr - 1, sc, color, src);
-            dfs(image, sr, sc + 1, color, src);
-            dfs(image, sr, sc - 1, color, src);
+        image[sr][sc] = color;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{sr, sc});
+        
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        while(!queue.isEmpty()){
+            int[] curr = queue.poll();
+            int row = curr[0];
+            int col = curr[1];
+                
+            for(int[] dir : dirs){
+                int nRow = row + dir[0];
+                int nCol = col + dir[1];
+                    
+                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && image[nRow][nCol] == org){
+                    image[nRow][nCol] = color;
+                    queue.add(new int[]{nRow, nCol});
+                }
+            }
         }
+        return image;
     }
 }
