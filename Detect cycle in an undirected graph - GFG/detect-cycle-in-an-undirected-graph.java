5c621f9b2df6c3f6ae07ac0ea0387437e8c27440
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-31 10:58:29 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] vis = new boolean[V];
+        
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                if(isCycle(i, -1, adj, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean isCycle(int node, int parent, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
+        vis[node] = true;
+        
+        for(int item : adj.get(node)){
+            if(vis[item] != true){
+                if(isCycle(item, node, adj, vis)){
+                    return true;
+                }
+            }else if(parent != item){
+                return true;
+            }
+        }
+        return false;
+    }
+}
