3b9219eb60e635c747a42d62f5f4532804254eeb
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-31 17:41:29 +0530

    Time: 1 ms (98.76%), Space: 44.5 MB (72.55%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,24 +2,26 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
+        
         boolean[][] vis = new boolean[n][m];
         
-        //checking for boundary zeroes
         for(int i = 0; i < n; i++){
             if(board[i][0] == 'O' && !vis[i][0]){
-                dfs(i, 0, board, vis);
+                dfs(i, 0, vis, board);
             }
+            
             if(board[i][m - 1] == 'O' && !vis[i][m - 1]){
-                dfs(i, m - 1, board, vis);
+                dfs(i, m - 1, vis, board);
             }
         }
         
         for(int j = 0; j < m; j++){
             if(board[0][j] == 'O' && !vis[0][j]){
-                dfs(0, j, board, vis);
+                dfs(0, j, vis, board);
             }
+            
             if(board[n - 1][j] == 'O' && !vis[n - 1][j]){
-                dfs(n - 1, j, board, vis);
+                dfs(n - 1, j, vis, board);
             }
         }
         
@@ -32,13 +34,13 @@ class Solution {
         }
     }
     
-    public void dfs(int i, int j, char[][] board, boolean[][] vis){
-        if(i >= 0 && i < board.length && j >= 0 && j < board[0].length && !vis[i][j] && board[i][j] == 'O'){
-            vis[i][j] = true;
-            dfs(i + 1, j, board, vis);
-            dfs(i - 1, j, board, vis);
-            dfs(i, j + 1, board, vis);
-            dfs(i, j - 1, board, vis);
+    public void dfs(int row, int col, boolean[][] vis, char[][] board){
+        if(row >= 0 && row < board.length && col >= 0 && col < board[0].length && !vis[row][col] && board[row][col] == 'O'){
+            vis[row][col] = true;
+            dfs(row + 1, col, vis, board);
+            dfs(row - 1, col, vis, board);
+            dfs(row, col + 1, vis, board);
+            dfs(row, col - 1, vis, board);
         }
     }
 }
