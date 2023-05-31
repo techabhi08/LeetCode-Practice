15dcaf02a3af8f3c382b6af44ebdd0fa92c9e7f9
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-05-31 20:44:21 +0530

    Time: 105 ms (50.83%), Space: 45.8 MB (36.35%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,6 +1,7 @@
-class Pair {
+class Pair{
     String word;
     int steps;
+    
     Pair(String word, int steps){
         this.word = word;
         this.steps = steps;
@@ -8,16 +9,16 @@ class Pair {
 }
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>();
+        Set<String> words = new HashSet<>();
         for(String word : wordList){
-            set.add(word);
+            words.add(word);
         }
         
         Queue<Pair> queue = new LinkedList<>();
         queue.add(new Pair(beginWord, 1));
         
-        if(set.contains(beginWord)){
-            set.remove(beginWord);
+        if(words.contains(beginWord)){
+            words.remove(beginWord);
         }
         
         while(!queue.isEmpty()){
@@ -34,13 +35,15 @@ class Solution {
                     char[] wordToReplace = word.toCharArray();
                     wordToReplace[i] = c;
                     String newWord = new String(wordToReplace);
-                    if(set.contains(newWord)){
-                        set.remove(newWord);
+                    
+                    if(words.contains(newWord)){
+                        words.remove(newWord);
                         queue.add(new Pair(newWord, steps + 1));
                     }
                 }
             }
         }
+        
         return 0;
     }
 }
