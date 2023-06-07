a29fe479fad1d27e0ba2fad715cb5da1a77b1e4a
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-07 22:48:19 +0530

    Time: 49 ms (91.27%), Space: 44 MB (26.28%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -2,46 +2,48 @@ class Pair{
     int row;
     int col;
     int dist;
+    
     Pair(int row, int col, int dist){
         this.row = row;
         this.col = col;
         this.dist = dist;
     }
 }
+
 class Solution {
     public int minimumEffortPath(int[][] heights) {
         int n = heights.length;
         int m = heights[0].length;
         
+        int[][] ans = new int[n][m];
+        for(int[] row : ans){
+            Arrays.fill(row, (int)1e8);
+        }
+        
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
         pq.add(new Pair(0, 0, 0));
+        ans[0][0] = 0;
         
-        int[][] dist = new int[n][m];
-        for(int[] rows : dist){
-            Arrays.fill(rows, (int)1e8);
-        }
-        dist[0][0] = 0;
-        
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
         
         while(!pq.isEmpty()){
             Pair curr = pq.poll();
             int row = curr.row;
             int col = curr.col;
-            int d = curr.dist;
+            int dist = curr.dist;
             
             if(row == n - 1 && col == m - 1){
-                return d;
+                return dist;
             }
             
             for(int[] dir : dirs){
-                int nRow = dir[0] + row;
-                int nCol = dir[1] + col;
+                int nRow = row + dir[0];
+                int nCol = col + dir[1];
                 
                 if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m){
-                    int effort = Math.max(d, Math.abs(heights[row][col] - heights[nRow][nCol]));
-                    if(effort < dist[nRow][nCol]){
-                        dist[nRow][nCol] = effort;
+                    int effort = Math.max(dist, Math.abs(heights[row][col] - heights[nRow][nCol]));
+                    if(effort < ans[nRow][nCol]){
+                        ans[nRow][nCol] = effort;
                         pq.add(new Pair(nRow, nCol, effort));
                     }
                 }
