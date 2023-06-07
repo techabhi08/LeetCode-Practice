d31de40e0d1d6aa8ebce2df45f17d4b3c955443b
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-07 19:52:48 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
@@ -27,71 +27,71 @@ class Main {
 }
 // } Driver Code Ends
 
-class Pair {
+
+//User function Template for Java
+class Pair{
     int node;
     int weight;
+    
     Pair(int node, int weight){
         this.node = node;
         this.weight = weight;
     }
 }
-//User function Template for Java
 class Solution {
 
 	public int[] shortestPath(int N,int M, int[][] edges) {
 		//Code here
-	List<List<Pair>> adj = new ArrayList<>();
-        for(int i = 0; i < N; i++){
-            adj.add(new ArrayList<>());
-        }
-        
-        for(int i = 0; i < edges.length; i++){
-            adj.get(edges[i][0]).add(new Pair(edges[i][1], edges[i][2]));
-        }
-        
-        Stack<Integer> stack = new Stack<>();
-        int[] vis = new int[N];
-        for(int i = 0; i < N; i++){
-            if(vis[i] == 0){
-                findToposort(i, adj, stack, vis);
-            }
-        }
-        
-        int[] dist = new int[N];
-        Arrays.fill(dist, (int)1e8);
-        dist[0] = 0;
-        
-        while(!stack.isEmpty()){
-            int top = stack.pop();
-            
-            for(Pair item : adj.get(top)){
-                int nDist = item.weight;
-                int node = item.node;
-                
-                if(nDist + dist[top] < dist[node]){
-                    dist[node] = nDist + dist[top];
-                }
-            }
-        }
-        
-        for(int i = 0; i < N; i++){
-            if(dist[i] == (int)1e8){
-                dist[i] = -1;
-            }
-        }
-        
-        return dist;
-    }
-    
-    public void findToposort(int node, List<List<Pair>> adj, Stack<Integer> stack, int[] vis){
-        vis[node] = 1;
-        
-        for(Pair item : adj.get(node)){
-            if(vis[item.node] == 0){
-                findToposort(item.node, adj, stack, vis);
-            }
-        }
-        
-        stack.add(node);
-    }
+		List<List<Pair>> adj = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++){
+		    adj.add(new ArrayList<>());
+		}
+		
+		for(int i = 0; i < M; i++){
+		    adj.get(edges[i][0]).add(new Pair(edges[i][1], edges[i][2]));
+		}
+		
+		Stack<Integer> stack = new Stack<>();
+		int[] vis = new int[N];
+		
+		for(int i = 0; i < N; i++){
+		    if(vis[i] == 0){
+		        findTopo(i, adj, vis, stack);
+		    }
+		}
+		
+		int[] dist = new int[N];
+		Arrays.fill(dist, (int)1e8);
+		dist[0] = 0;
+		
+		while(!stack.isEmpty()){
+		    int curr = stack.pop();
+		    
+		    for(Pair item : adj.get(curr)){
+		        if(item.weight + dist[curr] < dist[item.node]){
+		            dist[item.node] = item.weight + dist[curr];
+		        }
+		    }
+		}
+		
+		for(int i = 0; i < N; i++){
+		    if(dist[i] == (int)1e8){
+		        dist[i] = -1;
+		    }
+		}
+		return dist;
+	}
+	
+	public void findTopo(int node, List<List<Pair>> adj, int[] vis, Stack<Integer> stack){
+	    vis[node] = 1;
+	    
+	    for(Pair item : adj.get(node)){
+	        if(vis[item.node] == 0){
+	            findTopo(item.node, adj, vis, stack);
+	        }
+	    }
+	    
+	    stack.push(node);
+	}
 }
