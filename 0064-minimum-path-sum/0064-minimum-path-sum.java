d55e1f3d630d5abeeb559ad6e3cf3f9ccf03f655
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-15 13:09:57 +0530

    Time: 3 ms (29.05%), Space: 44.2 MB (73.19%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,28 +3,21 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }else{
+                    int left = 1000000, up = 1000000;
+                    if(j - 1 >= 0) left = grid[i][j] + dp[i][j - 1];
+                    if(i - 1 >= 0) up = grid[i][j] + dp[i - 1][j];
+
+                    dp[i][j] = Math.min(left, up);
+                }
+            }
         }
-        return findPath(n - 1, m - 1, grid, dp);
+        return dp[n - 1][m - 1];
     }
     
-    public int findPath(int i, int j, int[][] grid, int[][] dp){
-        if(i == 0 && j == 0){
-            return grid[i][j];
-        }
-        
-        if(i < 0 || j  < 0){
-            return 1000000;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int left = grid[i][j] + findPath(i, j - 1, grid, dp);
-        int up = grid[i][j] + findPath(i - 1, j, grid, dp);
-        
-        return dp[i][j] = Math.min(left, up);
-    }
 }
