118e332e3d9eb19a5bd56605c30bf27c638ce5d0
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-16 10:10:09 +0530

    Time: 5 ms (44.56%), Space: 44.1 MB (38.07%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -3,19 +3,21 @@ class Solution {
         int n = triangle.size();
         int m = triangle.get(n - 1).size();
         
-        int[][] dp = new int[n][m];
+        int[] next = new int[m];
         for(int i = 0; i < m; i++){
-            dp[n - 1][i] = triangle.get(n - 1).get(i);
+            next[i] = triangle.get(n - 1).get(i);
         }
         
         for(int row = n - 2; row >= 0; row--){
+            int[] curr = new int[m];
             for(int col = 0; col <= row; col++){
-                int down = triangle.get(row).get(col) + dp[row + 1][col];
-                int right = triangle.get(row).get(col) + dp[row + 1][col + 1];
+                int down = triangle.get(row).get(col) + next[col];
+                int right = triangle.get(row).get(col) + next[col + 1];
 
-                dp[row][col] = Math.min(down, right);
+                curr[col] = Math.min(down, right);
             }
+            next = curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 }
