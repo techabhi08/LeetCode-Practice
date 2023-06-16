378aaeeb90192b653ee5f743c85fcf6187804bdf
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-16 08:21:35 +0530

    Time: 5 ms (44.56%), Space: 44.4 MB (11.97%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -4,24 +4,18 @@ class Solution {
         int m = triangle.get(n - 1).size();
         
         int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return findMax(0, 0, triangle, dp);
-    }
-    
-    public int findMax(int row, int col, List<List<Integer>> triangle, int[][] dp){
-        if(row == triangle.size() - 1){
-            return triangle.get(row).get(col);
+        for(int i = 0; i < m; i++){
+            dp[n - 1][i] = triangle.get(n - 1).get(i);
         }
         
-        if(dp[row][col] != -1){
-            return dp[row][col];
+        for(int row = n - 2; row >= 0; row--){
+            for(int col = 0; col <= row; col++){
+                int down = triangle.get(row).get(col) + dp[row + 1][col];
+                int right = triangle.get(row).get(col) + dp[row + 1][col + 1];
+
+                dp[row][col] = Math.min(down, right);
+            }
         }
-        
-        int down = triangle.get(row).get(col) + findMax(row + 1, col, triangle, dp);
-        int right = triangle.get(row).get(col) + findMax(row + 1, col + 1, triangle, dp);
-        
-        return dp[row][col] = Math.min(down, right);
+        return dp[0][0];
     }
 }
