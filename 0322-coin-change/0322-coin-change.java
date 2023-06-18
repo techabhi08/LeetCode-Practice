820e21f0d7c59c48b9f77cd1f5a9a3253e1eedea
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-18 12:06:35 +0530

    Time: 21 ms (49.15%), Space: 44 MB (14.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,34 +1,26 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
+    public int coinChange(int[] coins, int amt) {
         int n = coins.length;
         Arrays.sort(coins);
-        int[][] dp = new int[n][amount + 1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n][amt + 1];
+        for(int i = 0; i <= amt; i++){
+            dp[0][i] = i % coins[0] == 0 ? i / coins[0] : (int)1e8;
         }
         
-        int ans = findMin(n - 1, amount, coins, dp);
+        for(int index = 1; index < n; index++){
+            for(int amount = 0; amount <= amt; amount++){
+                int notTake = dp[index - 1][amount];
+                int take = (int)1e8;
+                if(coins[index] <= amount){
+                    take = 1 + dp[index][amount - coins[index]];
+                }
+
+                dp[index][amount] = Math.min(take, notTake);
+            }
+        }
+        
+        int ans = dp[n - 1][amt];
         return ans == (int)1e8 ? -1 : ans;
     }
     
-    public int findMin(int index, int amount, int[] coins, int[][] dp){
-        if(index == 0){
-            if(amount % coins[0] == 0){
-                return amount / coins[0];
-            }
-            return (int)1e8;
-        }
-        
-        if(dp[index][amount] != -1){
-            return dp[index][amount];
-        }
-        
-        int notTake = findMin(index - 1, amount, coins, dp);
-        int take = (int)1e8;
-        if(coins[index] <= amount){
-            take = 1 + findMin(index, amount - coins[index], coins, dp);
-        }
-        
-        return dp[index][amount] = Math.min(take, notTake);
-    }
 }
