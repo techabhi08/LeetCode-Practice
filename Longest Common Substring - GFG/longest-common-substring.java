67bcd1213f54f3bc8f88a610ffbfb72e31df6035
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-19 13:05:51 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.java
+++ Longest Common Substring - GFG/longest-common-substring.java
@@ -32,18 +32,20 @@ class GFG
 class Solution{
     int longestCommonSubstr(String S1, String S2, int n, int m){
         // code here
-        int[][] dp = new int[n + 1][m + 1];
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
         
         int ans = 0;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(S1.charAt(i - 1) == S2.charAt(j - 1)){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                    ans = Math.max(ans, dp[i][j]);
+                    curr[j] = 1 + prev[j - 1];
+                    ans = Math.max(ans, curr[j]);
                 }else{
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 }
             }
+            prev = (int[])curr.clone();
         }
         return ans;
     }
