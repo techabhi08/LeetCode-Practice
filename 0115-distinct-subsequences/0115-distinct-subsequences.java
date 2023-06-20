a0334db7365a8c09a9f53b2b3494b370c8a90f0e
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-20 10:18:47 +0530

    Time: 24 ms (26.64%), Space: 44.5 MB (84.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,21 +3,21 @@ class Solution {
         int n = s.length();
         int m = t.length();
         
-        int[][] dp = new int[n + 1][m + 1];
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
         
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 1;
-        }
+        prev[0] = curr[0] = 1;
         
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(s.charAt(i - 1) == t.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    curr[j] = prev[j - 1] + prev[j];
                 }else{
-                    dp[i][j] = dp[i - 1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = (int[])curr.clone();
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
