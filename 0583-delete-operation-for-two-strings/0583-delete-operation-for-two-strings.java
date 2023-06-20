ce563b6e1d1809d0ed1cef599c3db901fc6ed879
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-20 08:43:48 +0530

    Time: 10 ms (74.79%), Space: 43.7 MB (49.29%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -2,26 +2,18 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n + 1][m + 1];
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
         
-        return (n + m) - (2 * findLCS(n - 1, m - 1, word1, word2, dp));
-    }
-    
-    public int findLCS(int n, int m, String word1, String word2, int[][] dp){
-        if(n < 0 || m < 0){
-            return 0;
-        }
-        
-        if(dp[n][m] != -1){
-            return dp[n][m];
-        }
-        
-        if(word1.charAt(n) == word2.charAt(m)){
-            return dp[n][m] = 1 + findLCS(n - 1, m - 1, word1, word2, dp);
-        }
-        return dp[n][m] = Math.max(findLCS(n - 1, m, word1, word2, dp), findLCS(n, m - 1, word1, word2, dp));
+        return (n + m) - (2 * dp[n][m]);
     }
 }
