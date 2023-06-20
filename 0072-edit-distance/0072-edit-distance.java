23d73cdb9a88db510c670943ce4a2f7ac128164f
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-20 19:39:38 +0530

    Time: 6 ms (69.55%), Space: 43.5 MB (56.86%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,25 +3,24 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         
-        int[][] dp = new int[n + 1][m + 1];
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-        }
-        
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
         for(int j = 0; j <= m; j++){
-            dp[0][j] = j;
+            prev[j] = j;
         }
         
         for(int i = 1; i <= n; i++){
+            curr[0] = i;
             for(int j = 1; j <= m; j++){
                 if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 }else{
-                    dp[i][j] = Math.min(1 + dp[i - 1][j], Math.min(1 + dp[i - 1][j - 1], 1 + dp[i][j - 1]));
+                    curr[j] = Math.min(1 + prev[j], Math.min(1 + prev[j - 1], 1 + curr[j - 1]));
                 }
             }
+            prev = (int[])curr.clone();
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 }
