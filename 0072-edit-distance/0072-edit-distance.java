5b929b5b80d08e128c5fce410b9630c5208f3aa9
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-20 19:30:13 +0530

    Time: 7 ms (35.34%), Space: 43.6 MB (50.97%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,29 +3,25 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         
-        int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n + 1][m + 1];
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = i;
         }
-        return findMin(n - 1, m - 1, word1, word2, dp);
+        
+        for(int j = 0; j <= m; j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.min(1 + dp[i - 1][j], Math.min(1 + dp[i - 1][j - 1], 1 + dp[i][j - 1]));
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
-    
-    public int findMin(int i, int j, String word1, String word2, int[][] dp){
-        if(i < 0){
-            return j + 1;
-        }
-        if(j < 0){
-            return i + 1;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        if(word1.charAt(i) == word2.charAt(j)){
-            return dp[i][j] = findMin(i - 1, j - 1, word1, word2, dp);
-        }
-        
-        return dp[i][j] = Math.min(1 + findMin(i - 1, j, word1, word2, dp), Math.min(1 + findMin(i - 1, j - 1, word1, word2, dp), 1 + findMin(i, j - 1, word1, word2, dp)));
-    } 
 }
