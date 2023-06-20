d2321d548914741df8d1253f391bea2d608d8ec4
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-20 10:17:27 +0530

    Time: 21 ms (67.59%), Space: 47 MB (68.50%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,29 +3,21 @@ class Solution {
         int n = s.length();
         int m = t.length();
         
-        int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return findLCS(n - 1, m - 1, s, t, dp);
-    }
-    
-    public int findLCS(int i, int j, String s, String t, int[][] dp){
-        if(j < 0){
-            return 1;
-        }
-        if(i < 0){
-            return 0;
+        int[][] dp = new int[n + 1][m + 1];
+        
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 1;
         }
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
-        
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = findLCS(i - 1, j - 1, s, t, dp) + findLCS(i - 1, j, s, t, dp);
-        }
-        
-        return dp[i][j] = findLCS(i - 1, j, s, t, dp);
+        return dp[n][m];
     }
 }
