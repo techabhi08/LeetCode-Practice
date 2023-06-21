6e92aff35a93a11b791c899902a9b637767a13c9
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-21 16:37:34 +0530

    Time: 44 ms (20.73%), Space: 54.9 MB (43.78%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -6,13 +6,9 @@ class Solution {
         int[] curr = new int[2];
         
         for(int index = n - 1; index >= 0; index--){
-            for(int canBuy = 0; canBuy <= 1; canBuy++){
-                if(canBuy == 1){
-                    curr[canBuy] = Math.max(-prices[index] + ahead[0], ahead[1]);
-                }else{
-                    curr[canBuy] = Math.max(prices[index] - fee + ahead[1], ahead[0]);
-                }
-            }
+            curr[1] = Math.max(-prices[index] + ahead[0], ahead[1]);
+            curr[0] = Math.max(prices[index] - fee + ahead[1], ahead[0]);
+            
             ahead = (int[])curr.clone();
         }
         return ahead[1];
