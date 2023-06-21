d48c093f7f7d6e0400a81c563b6a4ba75eb6bb00
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-21 11:29:39 +0530

    Time: 1 ms (94.54%), Space: 44.3 MB (54.12%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,25 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return findMax(0, 1, prices, dp);
-    }
-    
-    public int findMax(int index, int canBuy, int[] price, int[][] dp){
-        if(index >= price.length){
-            return 0;
-        }
+        int[][] dp = new int[n + 1][2];
+        dp[n][0] = 0;
+        dp[n][1] = 0;
         
-        if(dp[index][canBuy] != -1){
-            return dp[index][canBuy];
+        for(int index = n - 1; index >= 0; index--){
+            for(int canBuy = 0; canBuy <= 1; canBuy++){
+                if(canBuy == 1){
+                    dp[index][canBuy] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+                }else{
+                    dp[index][canBuy] = Math.max(prices[index] + dp[index + 1][1], dp[index + 1][0]);
+                }
+            }
         }
-        
-        if(canBuy == 1){
-            return dp[index][canBuy] = Math.max(-price[index] + findMax(index + 1, 0, price, dp), findMax(index + 1, 1, price, dp));
-        }
-        return dp[index][canBuy] = Math.max(price[index] + findMax(index + 1, 1, price, dp), findMax(index + 1, 0, price, dp));
+        return dp[0][1];
     }
 }
