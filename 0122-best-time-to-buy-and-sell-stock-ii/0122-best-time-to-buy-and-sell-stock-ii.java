72c7e91cce99b83bb3ca65389d22a78df1714dfc
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-21 11:32:54 +0530

    Time: 3 ms (11.54%), Space: 44.3 MB (54.12%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -6,13 +6,8 @@ class Solution {
         dp[n][1] = 0;
         
         for(int index = n - 1; index >= 0; index--){
-            for(int canBuy = 0; canBuy <= 1; canBuy++){
-                if(canBuy == 1){
-                    dp[index][canBuy] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
-                }else{
-                    dp[index][canBuy] = Math.max(prices[index] + dp[index + 1][1], dp[index + 1][0]);
-                }
-            }
+            dp[index][1] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+            dp[index][0] = Math.max(prices[index] + dp[index + 1][1], dp[index + 1][0]);
         }
         return dp[0][1];
     }
