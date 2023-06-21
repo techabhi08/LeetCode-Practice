de6c47aa3b0c76ed04eea695c25f8e4d8292cf95
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-21 14:56:55 +0530

    Time: 1 ms (83.05%), Space: 40.9 MB (59.55%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -4,14 +4,8 @@ class Solution {
         int[][] dp = new int[n + 1][2];
         
         for(int index = n - 1; index >= 0; index--){
-            for(int canBuy = 0; canBuy <= 1; canBuy++){
-                if(canBuy == 1){
-                    dp[index][canBuy] = Math.max(-prices[index] + dp[index + 1][0],
-                                   dp[index + 1][1]);
-                } else {
-                    dp[index][canBuy] = Math.max(prices[index] + (index + 2 <= n ? dp[index + 2][1] : 0), dp[index + 1][0]);
-                }
-            }
+            dp[index][1] = Math.max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+            dp[index][0] = Math.max(prices[index] + (index + 2 <= n ? dp[index + 2][1] : 0), dp[index + 1][0]);
         }
         return dp[0][1];
     }
