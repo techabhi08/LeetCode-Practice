a92d1529b61174c3746563a977b5ff2ed83be9e6
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-21 14:20:28 +0530

    Time: 2 ms (87.02%), Space: 41 MB (76.72%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -5,15 +5,9 @@ class Solution {
         int[][] curr = new int[2][k + 1];
         
         for(int index = n - 1; index >= 0; index--){
-            for(int canBuy = 0; canBuy <= 1; canBuy++){
-                for(int j = 1; j <= k; j++){
-                    if(canBuy == 1){
-                        curr[canBuy][j] = Math.max(-prices[index] + ahead[0][j], ahead[1][j]);
-                    }else{
-                        curr[canBuy][j] = Math.max(prices[index] + ahead[1][j - 1], 
-                                                   ahead[0][j]);
-                    }
-                }
+            for(int j = 1; j <= k; j++){
+                curr[1][j] = Math.max(-prices[index] + ahead[0][j], ahead[1][j]);
+                curr[0][j] = Math.max(prices[index] + ahead[1][j - 1], ahead[0][j]);
             }
             ahead = (int[][])curr.clone();
         }
