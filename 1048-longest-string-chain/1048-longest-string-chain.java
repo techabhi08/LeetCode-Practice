89697df7be7c81786f683a965f0145df703f5779
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-22 15:34:36 +0530

    Time: 67 ms (33.89%), Space: 43.7 MB (33.89%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int longestStrChain(String[] words) {
+        int n = words.length;
+        Arrays.sort(words, (x, y) -> x.length() - y.length());
+        
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        
+        int max = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(checkIsEqual(words[i], words[j]) && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        
+        return max;
+    }
+    
+    public boolean checkIsEqual(String word1, String word2){
+        if(word1.length() != word2.length() + 1){
+            return false;
+        }
+        
+        int i = 0, j = 0;
+        while(i < word1.length()){
+            if(j < word2.length() && word1.charAt(i) == word2.charAt(j)){
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        }
+        
+        if(i == word1.length() && j == word2.length()){
+            return true;
+        }
+        return false;
+    }
+}
