4d2a5491301696b7b713a122eef18a7dcd9d34f5
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-06-26 12:28:10 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (38.96%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,36 +1,36 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1;
+        
         List<Integer> ans = new ArrayList<>();
         
-        int rowS = 0;
-        int colS = 0;
-        int rowE = matrix.length - 1;
-        int colE = matrix[0].length - 1;
-        
-        while(rowS <= rowE && colS <= colE){
-            for(int i = colS; i <= colE; i++){
-                ans.add(matrix[rowS][i]);
+        while(rowStart <= rowEnd && colStart <= colEnd){
+            for(int i = colStart; i <= colEnd; i++){
+                ans.add(matrix[rowStart][i]);
             }
-            rowS++;
+            rowStart++;
             
-            for(int i = rowS; i <= rowE; i++){
-                ans.add(matrix[i][colE]);
+            for(int i = rowStart; i <= rowEnd; i++){
+                ans.add(matrix[i][colEnd]);
             }
-            colE--;
+            colEnd--;
             
-            if(rowS <= rowE){
-                for(int i = colE; i >= colS; i--){
-                    ans.add(matrix[rowE][i]);
+            if(rowStart <= rowEnd){
+                for(int i = colEnd; i >= colStart; i--){
+                    ans.add(matrix[rowEnd][i]);
                 }
             }
-            rowE--;
+            rowEnd--;
             
-            if(colS <= colE){
-                for(int i = rowE; i >= rowS; i--){
-                    ans.add(matrix[i][colS]);
+            if(colStart <= colEnd){
+                for(int i = rowEnd; i >= rowStart; i--){
+                    ans.add(matrix[i][colStart]);
                 }
             }
-            colS++;
+            colStart++;
         }
         return ans;
     }
