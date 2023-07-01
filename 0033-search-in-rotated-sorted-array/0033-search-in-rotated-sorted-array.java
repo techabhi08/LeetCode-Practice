9cd74f55507654a55a4686c8cd3a95e88c0987ee
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-01 08:16:16 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (51.41%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int peak = findPeak(nums);
+        int left = binarySearch(0, peak, nums, target);
+        if(left != -1){
+            return left;
+        }
+        return binarySearch(peak + 1, nums.length - 1, nums, target);
+    }
+    
+    public int binarySearch(int start, int end, int[] nums, int target){
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    public int findPeak(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(mid > start && nums[mid] < nums[mid - 1]){
+                return mid - 1;
+            }else if(mid < end && nums[mid] > nums[mid + 1]){
+                return mid;
+            }else if(nums[mid] <= nums[start]){
+                end = mid - 1;
+            }else{
+              start = mid + 1;  
+            }
+        }
+        return end;
+    }
+}
