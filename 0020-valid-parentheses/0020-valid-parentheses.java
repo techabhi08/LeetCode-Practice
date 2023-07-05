5751639390a29dea7fc656f49f1d7202641860bf
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-05 16:37:23 +0530

    Time: 2 ms (91.75%), Space: 40.7 MB (40.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else if(c == ')' && stack.pop() != '('){
+                    return false;
+                }else if(c == '}' && stack.pop() != '{'){
+                    return false;
+                }else if(c == ']' && stack.pop() != '['){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
