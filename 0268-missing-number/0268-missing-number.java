2f42c2de20f1e92b9aeab806f3e91a6b588573bf
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-10 15:19:00 +0530

    Time: 1 ms (41.38%), Space: 44.3 MB (51.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,7 +3,7 @@ class Solution {
         int i = 0;
         while(i < nums.length){
             int correct = nums[i];
-            if(nums[i] < nums.length && nums[i] != nums[correct]){
+            if(correct < nums.length && nums[i] != nums[correct]){
                 int temp = nums[i];
                 nums[i] = nums[correct];
                 nums[correct] = temp;
