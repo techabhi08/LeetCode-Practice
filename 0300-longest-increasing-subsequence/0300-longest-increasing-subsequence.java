6274824322d0cc2adacb2f74a123ade0e15b36a1
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-10 20:19:23 +0530

    Time: 8 ms (77.27%), Space: 43.1 MB (78.30%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,30 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
         
-        for(int index = n - 1; index >= 0; index--){
-            for(int prev = index - 1; prev >= -1; prev--){
-                int take = 0;
-                if(prev == -1 || nums[prev] < nums[index]){
-                    take = 1 + dp[index + 1][index + 1];
-                }
-                int notTake = dp[index + 1][prev + 1];
-
-                dp[index][prev + 1] = Math.max(take, notTake);
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > temp.get(temp.size() - 1)){
+                temp.add(nums[i]);
+            }else{
+                int index = findIndex(0, temp.size() - 1, nums[i], temp);
+                temp.set(index, nums[i]);
             }
         }
-        return dp[0][0];
+        return temp.size();
+    }
+    
+    public int findIndex(int start, int end, int target, List<Integer> temp){
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(temp.get(mid) == target){
+                return mid;
+            }else if(temp.get(mid) <= target){
+                start = mid + 1;
+            }else{
+                end = mid;
+            }
+        }
+        return end;
     }
 }
