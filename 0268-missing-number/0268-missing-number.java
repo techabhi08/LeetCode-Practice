63347eb2912b470517355d21fee17559b9aaefd1
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-10 15:25:38 +0530

    Time: 0 ms (100.00%), Space: 44 MB (71.03%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int xor = 0;
         int i = 0;
         while(i < nums.length){
-            int correct = nums[i];
-            if(correct < nums.length && nums[i] != nums[correct]){
-                int temp = nums[i];
-                nums[i] = nums[correct];
-                nums[correct] = temp;
-            }else{
-                i++;
-            }
+            xor = xor ^ i ^ nums[i];
+            i++;
         }
-        
-        for(i = 0; i < nums.length; i++){
-            if(nums[i] != i){
-                return i;
-            }
-        }
-        return nums.length;
+        return xor ^ i;
     }
 }
