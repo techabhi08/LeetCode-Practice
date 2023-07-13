23167a139679bcf828ac8e0e2d0b9f1d59237a49
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-13 09:08:53 +0530

    Time: 3 ms (95.24%), Space: 43.9 MB (79.71%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -10,32 +10,32 @@ class Solution {
             adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
         
-        int[] indegree = new int[numCourses];
+        int[] vis = new int[numCourses];
+        int[] pathVis = new int[numCourses];
+        
         for(int i = 0; i < numCourses; i++){
-            for(int item : adj.get(i)){
-                indegree[item]++;
+            if(isCycle(i, vis, pathVis, adj)){
+                return false;
             }
         }
+        return true;
+    }
+    
+    public boolean isCycle(int node, int[] vis, int[] pathVis, List<List<Integer>> adj){
+        vis[node] = 1;
+        pathVis[node] = 1;
         
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < numCourses; i++){
-            if(indegree[i] == 0){
-                queue.add(i);
-            }
-        }
-        
-        int i = 0;
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            i++;
-            for(int item : adj.get(curr)){
-                indegree[item]--;
-                if(indegree[item] == 0){
-                    queue.add(item);
+        for(int item : adj.get(node)){
+            if(vis[item] == 0){
+                if(isCycle(item, vis, pathVis, adj)){
+                    return true;
                 }
+            }else if(pathVis[item] == 1){
+                return true;
             }
         }
         
-        return i == numCourses;
+        pathVis[node] = 0;
+        return false;
     }
 }
