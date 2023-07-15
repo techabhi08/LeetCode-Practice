4b347ff98bccb3ae291b7bcc4b0405e1d41b7736
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 21:34:41 +0530

    Time: 1 ms (99.96%), Space: 44.7 MB (6.34%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,23 +1,25 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int m = triangle.get(n - 1).size();
-        
-        int[] next = new int[m];
-        for(int i = 0; i < m; i++){
-            next[i] = triangle.get(n - 1).get(i);
+        int[][] dp = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return findMin(0, 0, triangle, dp);
+    }
+    
+    public int findMin(int row, int col, List<List<Integer>> triangle, int[][] dp){
+        if(row == triangle.size() - 1){
+            return triangle.get(row).get(col);
         }
         
-        for(int row = n - 2; row >= 0; row--){
-            int[] curr = new int[m];
-            for(int col = 0; col <= row; col++){
-                int down = triangle.get(row).get(col) + next[col];
-                int right = triangle.get(row).get(col) + next[col + 1];
-
-                curr[col] = Math.min(down, right);
-            }
-            next = curr;
+        if(dp[row][col] != -1){
+            return dp[row][col];
         }
-        return next[0];
+        
+        int down = triangle.get(row).get(col) + findMin(row + 1, col, triangle, dp);
+        int diag = triangle.get(row).get(col) + findMin(row + 1, col + 1, triangle, dp);
+        
+        return dp[row][col] = Math.min(down, diag);
     }
 }
