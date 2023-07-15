d40b6376d7d4163273253e9ff697053de128df46
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 09:58:34 +0530

    Time: 2 ms (88.48%), Space: 41.7 MB (35.43%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -17,24 +17,23 @@ class Solution {
         }
         
         int mins = 0;
-        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
         while(!queue.isEmpty() && fresh != 0){
             int size = queue.size();
             mins++;
             
             for(int i = 0; i < size; i++){
                 int[] curr = queue.poll();
-                int row = curr[0];
-                int col = curr[1];
-                
+            
                 for(int[] dir : dirs){
-                    int nRow = row + dir[0];
-                    int nCol = col + dir[1];
+                    int nRow = curr[0] + dir[0];
+                    int nCol = curr[1] + dir[1];
                     
                     if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == 1){
-                        fresh--;
-                        queue.add(new int[]{nRow, nCol});
                         grid[nRow][nCol] = 2;
+                        queue.add(new int[]{nRow, nCol});
+                        fresh--;
                     }
                 }
             }
