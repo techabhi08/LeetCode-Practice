bfc0f776f62db2185545c7c5f6049e80dee884c4
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 15:55:18 +0530

    Time: 17 ms (60.26%), Space: 44.7 MB (78.72%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,14 +2,14 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
         
-        if(grid.length == 0 || grid == null || grid[0][0] == 1 || grid[n - 1][n - 1] == 1){
+        if(n == 0 || grid == null || grid[0][0] == 1 || grid[n - 1][n - 1] == 1){
             return -1;
         }
         
-        int[][] vis = new int[n][n];
+        boolean[][] vis = new boolean[n][n];
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0, 0});
-        vis[0][0] = 1;
+        vis[0][0] = true;
         
         int ans = 0;
         while(!queue.isEmpty()){
@@ -29,10 +29,9 @@ class Solution {
                     for(int k = -1; k <= 1; k++){
                         int nRow = row + j;
                         int nCol = col + k;
-                        
-                        if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < n && vis[nRow][nCol] == 0 && grid[nRow][nCol] == 0){
+                        if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < n && !vis[nRow][nCol] && grid[nRow][nCol] == 0){
                             queue.add(new int[]{nRow, nCol});
-                            vis[nRow][nCol] = 1;
+                            vis[nRow][nCol] = true;
                         }
                     }
                 }
