83db27ec258e0a2631c71dd4976817ca883947e6
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 21:30:03 +0530

    Time: 3 ms (30.93%), Space: 43.7 MB (88.89%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,26 +3,20 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return findMax(n - 1, m - 1, grid, dp);
-    }
-    
-    public int findMax(int i, int j, int[][] grid, int[][] dp){
-        if(i < 0 || j < 0){
-            return 1000000;
-        }
-        if(i == 0 && j == 0){
-            return grid[i][j];
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
         
-        int left = grid[i][j] + findMax(i, j - 1, grid, dp);
-        int up = grid[i][j] + findMax(i - 1, j, grid, dp);
-        
-        return dp[i][j] = Math.min(left, up);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }else{
+                    int left = 1000000, up = 1000000;
+                    if(j - 1 >= 0) left = grid[i][j] + dp[i][j - 1];
+                    if(i - 1 >= 0) up = grid[i][j] + dp[i - 1][j];
+                    
+                    dp[i][j] = Math.min(left, up);
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
     }
 }
