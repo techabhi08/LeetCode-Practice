c167e3a745a9603c259430fd0ac6e5c99a0cd783
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 10:04:14 +0530

    Time: 1 ms (48.11%), Space: 44 MB (70.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -2,32 +2,31 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length;
         int m = image[0].length;
-        int org = image[sr][sc];
         
+        int org = image[sr][sc];
         if(org == color){
             return image;
         }
-        image[sr][sc] = color;
+        
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{sr, sc});
+        image[sr][sc] = color;
         
         int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
         while(!queue.isEmpty()){
             int[] curr = queue.poll();
-            int row = curr[0];
-            int col = curr[1];
+            
+            for(int dir[] : dirs){
+                int nRow = curr[0] + dir[0];
+                int nCol = curr[1] + dir[1];
                 
-            for(int[] dir : dirs){
-                int nRow = row + dir[0];
-                int nCol = col + dir[1];
-                    
                 if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && image[nRow][nCol] == org){
                     image[nRow][nCol] = color;
                     queue.add(new int[]{nRow, nCol});
                 }
             }
         }
+        
         return image;
     }
 }
