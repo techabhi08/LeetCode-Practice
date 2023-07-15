b3114ee8f0e0a00749a60b091a01a839853f3cc5
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 10:25:57 +0530

    Time: 1 ms (98.52%), Space: 44.3 MB (37.81%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,25 +2,28 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-        
         boolean[][] vis = new boolean[n][m];
         
         for(int i = 0; i < n; i++){
-            if(board[i][0] == 'O' && !vis[i][0]){
+            //checking for 0th col
+            if(!vis[i][0] && board[i][0] == 'O'){
                 dfs(i, 0, vis, board);
             }
             
-            if(board[i][m - 1] == 'O' && !vis[i][m - 1]){
+            //checking for last col
+            if(!vis[i][m - 1] && board[i][m - 1] == 'O'){
                 dfs(i, m - 1, vis, board);
             }
         }
         
         for(int j = 0; j < m; j++){
-            if(board[0][j] == 'O' && !vis[0][j]){
+            //checking for 0th row
+            if(!vis[0][j] && board[0][j] == 'O'){
                 dfs(0, j, vis, board);
             }
             
-            if(board[n - 1][j] == 'O' && !vis[n - 1][j]){
+            //checking for last row
+            if(!vis[n - 1][j] && board[n - 1][j] == 'O'){
                 dfs(n - 1, j, vis, board);
             }
         }
@@ -35,7 +38,7 @@ class Solution {
     }
     
     public void dfs(int row, int col, boolean[][] vis, char[][] board){
-        if(row >= 0 && row < board.length && col >= 0 && col < board[0].length && !vis[row][col] && board[row][col] == 'O'){
+        if(row >= 0 && row < board.length && col >= 0 && col < board[0].length && board[row][col] == 'O' && !vis[row][col]){
             vis[row][col] = true;
             dfs(row + 1, col, vis, board);
             dfs(row - 1, col, vis, board);
