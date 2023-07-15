545053ff4d1e01b4b54876a1d2fa3204817d5022
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 19:37:05 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (41.92%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        dp[0][0] = 1;
+        int[] prev = new int[n];
+        int[] curr = new int[n];
+        prev[0] = 1;
+        curr[0] = 1;
         
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(i == 0 && j == 0){
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }else{
                     int left = 0, up = 0;
-                    if(j - 1 >= 0)left = dp[i][j - 1];
-                    if(i - 1 >= 0) up = dp[i - 1][j];
+                    if(j - 1 >= 0)left = curr[j - 1];
+                    if(i - 1 >= 0) up = prev[j];
 
-                    dp[i][j] = left + up;
+                    curr[j] = left + up;
                 }
             }
+            prev = (int[])curr.clone();
         }
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }
 }
