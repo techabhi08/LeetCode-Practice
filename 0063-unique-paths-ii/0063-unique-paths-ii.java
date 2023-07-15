69c9c75d12532d3cd99a49c4ec068cf021056f4c
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-15 19:44:55 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (11.79%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,29 +1,28 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
         int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return findMax(n - 1, m - 1, obstacleGrid, dp);
-    }
-    
-    public int findMax(int i, int j, int[][] grid, int[][] dp){
-        if(i < 0 || j < 0 || grid[i][j] == 1){
-            return 0;
-        }
-        if(i == 0 && j == 0){
-            return 1;
+        if(grid[0][0] == 0){
+            dp[0][0] = 1;
         }
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0 && grid[i][j] == 0){
+                    dp[i][j] = 1;
+                }
+                else if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else{
+                    int left = 0, up = 0;
+                    if(j - 1 >= 0) left = dp[i][j - 1];
+                    if(i - 1 >= 0) up = dp[i - 1][j];
+                    
+                    dp[i][j] = left + up;
+                }
+            }
         }
-        
-        int left = findMax(i, j - 1, grid, dp);
-        int up = findMax(i - 1, j, grid, dp);
-        
-        return dp[i][j] = left + up;
+        return dp[n - 1][m - 1];
     }
 }
