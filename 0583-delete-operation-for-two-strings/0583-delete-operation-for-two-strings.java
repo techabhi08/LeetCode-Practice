d62e45cd794acabbcba9db546d9ffae4e80bb5c9
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-16 20:14:21 +0530

    Time: 10 ms (80.52%), Space: 43.7 MB (58.44%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -2,20 +2,27 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[] prev = new int[m + 1];
-        int[] curr = new int[m + 1];
-        
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-                    curr[j] = 1 + prev[j - 1];
-                }else{
-                    curr[j] = Math.max(prev[j], curr[j - 1]);
-                }
-            }
-            prev = (int[])curr.clone();
+        int[][] dp = new int[n][m];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
         
-        return (n + m) - (2 * prev[m]);
+        return (n + m) - 2 * findMax(n - 1, m - 1, word1, word2, dp);
+    }
+    
+    public int findMax(int i, int j, String word1, String word2, int[][] dp){
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(word1.charAt(i) == word2.charAt(j)){
+            return dp[i][j] = 1 + findMax(i - 1, j - 1, word1, word2, dp);
+        }
+        return dp[i][j] = Math.max(findMax(i - 1, j, word1, word2, dp), 
+                        findMax(i, j - 1, word1, word2, dp));
     }
 }
