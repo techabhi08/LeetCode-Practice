d9f8644678c1f0081d944482e62de721044a9714
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-16 19:34:53 +0530

    Time: 43 ms (45.19%), Space: 45 MB (37.17%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,28 +1,19 @@
 class Solution {
-    public int minInsertions(String s) {
-        int n = s.length();
-        StringBuilder sb = new StringBuilder(s);
+    public int minInsertions(String s1) {
+        int n = s1.length();
+        StringBuilder sb = new StringBuilder(s1);
         String s2 = sb.reverse().toString();
-        int[][] dp = new int[n][n];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
+        int[][] dp = new int[n + 1][n + 1];
         
-        return n - findMax(n - 1, n - 1, s, s2, dp);
-    }
-    
-    public int findMax(int i, int j, String s1, String s2, int[][] dp){
-        if(i < 0 || j < 0){
-            return 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = 1 + findMax(i - 1, j - 1, s1, s2, dp);
-        }
-        return dp[i][j] = Math.max(findMax(i - 1, j, s1, s2, dp), findMax(i, j - 1, s1, s2, dp));
+        return n - dp[n][n];
     }
 }
