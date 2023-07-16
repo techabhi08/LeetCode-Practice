72ac7dce468319095cd00bf98dcd36a93a7f30fa
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-16 11:39:03 +0530

    Time: 22 ms (49.04%), Space: 44.1 MB (18.86%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
+        int[] prev = new int[amount + 1];
+        int[] curr = new int[amount + 1];
         for(int i = 1; i <= amount; i++){
-            dp[0][i] = i % coins[0] == 0 ? i / coins[0] : (int)1e8;
+            prev[i] = i % coins[0] == 0 ? i / coins[0] : (int)1e8;
         }
+        
         
         for(int index = 1; index < n; index++){
             for(int target = 1; target <= amount; target++){
-                int notTake = dp[index - 1][target];
+                int notTake = prev[target];
                 int take = (int)1e8;
                 if(coins[index] <= target){
-                    take = 1 + dp[index][target - coins[index]];
+                    take = 1 + curr[target - coins[index]];
                 }
 
-                dp[index][target] = Math.min(take, notTake);
+                curr[target] = Math.min(take, notTake);
             }
+            prev = (int[])curr.clone();
         }
-        int ans = dp[n - 1][amount];
+        int ans = prev[amount];
         return ans == (int)1e8 ? -1 : ans;
     }
 }
