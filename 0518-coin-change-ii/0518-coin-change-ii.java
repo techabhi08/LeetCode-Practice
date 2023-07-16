1a2266046bbc4e550a4c06aae52d7442cab7812e
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-16 17:02:55 +0530

    Time: 6 ms (67.36%), Space: 46.6 MB (9.78%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,32 @@
 class Solution {
-    public int change(int amt, int[] coins) {
-        int n = coins.length;
-        
-        int[][] dp = new int[n][amt + 1];
-        for(int i = 0; i <= amt; i++){
-            dp[0][i] = i % coins[0] == 0 ? 1 : 0;
+    public int change(int amount, int[] coins) {
+        int n = coins.length; 
+        int[][] dp = new int[n][amount + 1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return findCount(n - 1, amount, coins, dp);
+    }
+    
+    public int findCount(int index, int amount, int[] coins, int[][] dp){
+        if(index == 0){
+            return amount % coins[0] == 0 ? 1 : 0;
         }
         
-        for(int index = 1; index < n; index++){
-            for(int amount = 0; amount <= amt; amount++){
-                int notTake = dp[index - 1][amount];
-                int take = 0;
-                if(coins[index] <= amount){
-                    take = dp[index][amount - coins[index]];
-                }
-
-                dp[index][amount] = take + notTake;
-            }
+        if(amount == 0){
+            return 1;
         }
-        return dp[n - 1][amt];
+        
+        if(dp[index][amount] != -1){
+            return dp[index][amount];
+        }
+        
+        int notTake = findCount(index - 1, amount, coins, dp);
+        int take = 0;
+        if(coins[index] <= amount){
+            take = findCount(index, amount - coins[index], coins, dp);
+        }
+        
+        return dp[index][amount] = take + notTake;
     }
 }
