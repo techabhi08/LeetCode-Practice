7aea4e53c2a685cf46a518f1820e323d4b0848fe
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-16 19:10:08 +0530

    Time: 43 ms (22.33%), Space: 48.5 MB (48.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,10 +6,10 @@ class Solution {
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
-        return findLongest(n - 1, m - 1, text1, text2, dp);
+        return findMax(n - 1, m - 1, text1, text2, dp);
     }
     
-    public int findLongest(int i, int j, String text1, String text2, int[][] dp){
+    public int findMax(int i, int j, String text1, String text2, int[][] dp){
         if(i < 0 || j < 0){
             return 0;
         }
@@ -19,9 +19,8 @@ class Solution {
         }
         
         if(text1.charAt(i) == text2.charAt(j)){
-            return dp[i][j] = 1 + findLongest(i - 1, j - 1, text1, text2, dp);
+            return dp[i][j] = 1 + findMax(i - 1, j - 1, text1, text2, dp);
         }
-        
-        return dp[i][j] = Math.max(findLongest(i - 1, j, text1, text2, dp), findLongest(i, j - 1, text1, text2, dp));
+        return dp[i][j] = Math.max(findMax(i - 1, j, text1, text2, dp), findMax(i, j - 1, text1, text2, dp));
     }
 }
