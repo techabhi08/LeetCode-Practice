b107e2b3c97457f99614b12a3e6a018dccf57c76
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-16 19:32:16 +0530

    Time: 59 ms (11.23%), Space: 45.1 MB (37.17%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -3,20 +3,26 @@ class Solution {
         int n = s.length();
         StringBuilder sb = new StringBuilder(s);
         String s2 = sb.reverse().toString();
-        
-        int[] prev = new int[n + 1];
-        int[] curr = new int[n + 1];
-        
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if(s.charAt(i - 1) == s2.charAt(j - 1)){
-                    curr[j] = 1 + prev[j - 1];
-                }else{
-                    curr[j] = Math.max(prev[j], curr[j - 1]);
-                }
-            }
-            prev = (int[])curr.clone();
+        int[][] dp = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
-        return n - prev[n];
+        
+        return n - findMax(n - 1, n - 1, s, s2, dp);
+    }
+    
+    public int findMax(int i, int j, String s1, String s2, int[][] dp){
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = 1 + findMax(i - 1, j - 1, s1, s2, dp);
+        }
+        return dp[i][j] = Math.max(findMax(i - 1, j, s1, s2, dp), findMax(i, j - 1, s1, s2, dp));
     }
 }
