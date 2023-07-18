fa330e2df4ba0e9b697fa213b33eeb014adf4468
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-18 12:22:09 +0530

    Time: 9 ms (81.91%), Space: 45.3 MB (87.08%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         List<int[]> ans = new ArrayList<>();
-        int index = 0;
-        
         Arrays.sort(intervals, (x, y) -> x[0] - y[0]);
+        ans.add(intervals[0]);
         
-        while(index < intervals.length){
-            if(ans.size() == 0 || ans.get(ans.size() - 1)[1] < intervals[index][0]){
-                ans.add(new int[]{intervals[index][0], intervals[index][1]});
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] > ans.get(ans.size() - 1)[1]){
+                ans.add(intervals[i]);
             }else{
-                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[index][1]);
+                int[] last = ans.get(ans.size() - 1);
+                last[1] = Math.max(last[1], intervals[i][1]);
             }
-            index++;
         }
         
-        int[][] arr = new int[ans.size()][2];
+        int[][] res = new int[ans.size()][2];
         int i = 0;
-        for(int[] interval : ans){
-            arr[i++] = interval;
+        for(int[] item : ans){
+            res[i++] = item;
         }
-        return arr;
+        return res;
     }
 }
