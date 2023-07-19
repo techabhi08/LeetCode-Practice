ea589d61c490363a26a5671a01fd43939cf634ad
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-19 22:03:53 +0530

    Time: 1 ms (99.81%), Space: 48.4 MB (58.50%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int highest = Integer.MAX_VALUE;
         int count = 0;
+        int max = -1;
         
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == highest){
+            if(nums[i] == max){
                 count++;
             }else if(count == 0){
-                highest = nums[i];
+                max = nums[i];
             }else{
                 count--;
             }
@@ -15,14 +15,11 @@ class Solution {
         
         count = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == highest){
+            if(nums[i] == max){
                 count++;
             }
         }
         
-        if(count > nums.length / 2){
-            return highest;
-        }
-        return -1;
+        return count > (nums.length / 2) ? max : -1;
     }
 }
