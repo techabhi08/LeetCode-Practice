d64f4b3fc25ade5a80c9ddab01c8063f7ed64a12
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-20 13:21:38 +0530

    Time: 2 ms (34.89%), Space: 43.8 MB (61.00%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -2,16 +2,16 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         Set<List<Integer>> ans = new HashSet<>();
-        findSets(0, nums, new ArrayList<>(), ans);
+        findSets(0, nums, ans, new ArrayList<>());
         
         List<List<Integer>> res = new ArrayList<>();
-        for(List<Integer> inner : ans){
-            res.add(new ArrayList<>(inner));
+        for(List<Integer> item : ans){
+            res.add(new ArrayList<>(item));
         }
         return res;
     }
     
-    public void findSets(int index, int[] nums, List<Integer> inner, Set<List<Integer>> ans){
+    public void findSets(int index, int[] nums, Set<List<Integer>> ans, List<Integer> inner){
         if(index == nums.length){
             ans.add(new ArrayList<>(inner));
             return;
@@ -19,9 +19,9 @@ class Solution {
         
         if(index < nums.length){
             inner.add(nums[index]);
-            findSets(index + 1, nums, inner, ans);
+            findSets(index + 1, nums, ans, inner);
             inner.remove(inner.size() - 1);
-            findSets(index + 1, nums, inner, ans);
+            findSets(index + 1, nums, ans, inner);
         }
     }
 }
