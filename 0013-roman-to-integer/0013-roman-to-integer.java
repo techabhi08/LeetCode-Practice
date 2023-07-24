585ea26272d84b5c37ca29db0e9863798b1b38a3
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-24 09:52:48 +0530

    Time: 4 ms (94.94%), Space: 43.7 MB (65.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,32 +2,33 @@ class Solution {
     public int romanToInt(String s) {
         int ans = 0;
         for(int i = 0; i < s.length(); i++){
-            int val = findNum(s.charAt(i));
-            if(i + 1 < s.length() && val < findNum(s.charAt(i + 1))){
-                ans -= val;
+            char ch = s.charAt(i);
+            int value = roman(ch);
+            if(i + 1 < s.length() && roman(s.charAt(i + 1)) > value){
+                ans -= value;
             }else{
-                ans += val;
+                ans += value;
             }
         }
         return ans;
     }
     
-    public int findNum(char c){
-        switch(c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
+    public int roman(char c){
+        switch(c){
             case 'M':
                 return 1000;
+            case 'D':
+                return 500;
+            case 'C':
+                return 100;
+            case 'L':
+                return 50;
+            case 'X':
+                return 10;
+            case 'V':
+                return 5;
+            case 'I':
+                return 1;
             default:
                 return 0;
         }
