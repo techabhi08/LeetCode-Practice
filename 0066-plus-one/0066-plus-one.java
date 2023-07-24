25037a5b812fc477a208caa1a9b1a2f676f051cb
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-24 12:46:25 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (67.33%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            }else{
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
