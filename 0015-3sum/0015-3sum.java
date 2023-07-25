0bfa61c76ee70d3e24993ee17ee4fd29ba9862cf
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-25 09:14:33 +0530

    Time: 43 ms (64.10%), Space: 51.4 MB (40.97%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,9 +7,9 @@ class Solution {
             if(i > 0 && nums[i] == nums[i - 1]){
                 continue;
             }
+            int target = -nums[i];
             int j = i + 1;
             int k = nums.length - 1;
-            int target = -nums[i];
             
             while(j < k){
                 if(nums[j] + nums[k] == target){
@@ -20,7 +20,6 @@ class Solution {
                     while(j < k && nums[j] == nums[j - 1]){
                         j++;
                     }
-                    
                     while(j < k && nums[k] == nums[k + 1]){
                         k--;
                     }
