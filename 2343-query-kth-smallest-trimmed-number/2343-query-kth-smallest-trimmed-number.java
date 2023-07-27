9166a2c949c83dc96d0f0781d9d58f7a0c3830a9
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-27 16:07:24 +0530

    Time: 174 ms (67.50%), Space: 45.8 MB (14.17%) - LeetHub

---
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.java
@@ -0,0 +1,33 @@
+class Node{
+    int index;
+    String val;
+    
+    Node(int index, String val){
+        this.index = index;
+        this.val = val;
+    }
+}
+
+class Solution {
+    public int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
+        HashMap<Integer, Node[]> map = new HashMap<>();
+        int[] res = new int[queries.length];
+        int index = 0;
+        int len = nums[0].length();
+        
+        for(int[] query : queries){
+            if(!map.containsKey(query[1])){
+                Node[] arr = new Node[nums.length];
+                for(int i = 0; i < nums.length; i++){
+                    String str = nums[i].substring(len - query[1], len);
+                    arr[i] = new Node(i, str);
+                }
+                Arrays.sort(arr, (a, b) -> a.val.compareTo(b.val));
+                map.put(query[1], arr);
+            }
+            res[index++] = map.get(query[1])[query[0] - 1].index;
+        }
+        
+        return res;
+    }
+}
