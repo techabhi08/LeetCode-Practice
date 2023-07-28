3f010213d210f21224983b014055b67eb683621c
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-07-28 16:18:29 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (92.43%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.java
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -10,48 +10,47 @@
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
         int n = mountainArr.length();
-        int peakIndex = findPeak(mountainArr);
+        int peakIndex = findPeak(mountainArr, n);
         
-        int ans = binarySearch(0, peakIndex, mountainArr, target);
+        int ans = findIndex(0, peakIndex, mountainArr, target);
         if(ans != -1){
             return ans;
         }
-        return binarySearch(peakIndex + 1, n - 1, mountainArr, target);
+        return findIndex(peakIndex + 1, n - 1, mountainArr, target);
     }
     
-    public int binarySearch(int start, int end, MountainArray m, int target){
-        boolean isAsc = m.get(start) < m.get(end);
-        
+    public int findIndex(int start, int end, MountainArray arr, int target){
+        boolean isAsc = arr.get(start) < arr.get(end);
         while(start <= end){
             int mid = start + (end - start) / 2;
             if(isAsc){
-                if(m.get(mid) < target){
+                if(arr.get(mid) < target){
                     start = mid + 1;
                 }else{
                     end = mid - 1;
                 }
             }else{
-                if(m.get(mid) < target){
+                if(arr.get(mid) < target){
                     end = mid - 1;
                 }else{
                     start = mid + 1;
                 }
             }
             
-            if(m.get(mid) == target){
+            if(arr.get(mid) == target){
                 return mid;
             }
         }
         return -1;
     }
     
-    public int findPeak(MountainArray m){
+    public int findPeak(MountainArray arr, int n){
         int start = 0;
-        int end = m.length() - 1;
+        int end = n - 1;
         
         while(start < end){
             int mid = start + (end - start) / 2;
-            if(m.get(mid) > m.get(mid + 1)){
+            if(arr.get(mid) > arr.get(mid + 1)){
                 end = mid;
             }else{
                 start = mid + 1;
