281c7cb2eaa9fe839e12fe1643ab9c9ede01068d
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-08-08 09:04:29 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (96.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-        int ans = binarySearch(0, pivot, nums, target);
-        if(ans != -1){
-            return ans;
+        int peak = findPeak(nums);
+        int ans = binarySearch(0, peak, nums, target);
+        if(ans == - 1){
+            return binarySearch(peak + 1, nums.length - 1, nums, target);
         }
-        return binarySearch(pivot + 1, nums.length - 1, nums, target);
+        return ans;
     }
     
     public int binarySearch(int start, int end, int[] nums, int target){
@@ -22,22 +22,22 @@ class Solution {
         return -1;
     }
     
-    public int findPivot(int[] nums){
+    public int findPeak(int[] nums){
         int start = 0;
         int end = nums.length - 1;
         
-        while(start <= end){
+        while(start < end){
             int mid = start + (end - start) / 2;
-            if(end > mid && nums[mid] > nums[mid + 1]){
-                return mid;
-            }else if(mid > start && nums[mid] < nums[mid - 1]){
+            if(mid > start && nums[mid] < nums[mid - 1]){
                 return mid - 1;
-            }else if(nums[start] >= nums[mid]){
+            }else if(mid < end && nums[mid] > nums[mid + 1]){
+                return mid;
+            }else if(nums[mid] <= nums[start]){ 
                 end = mid - 1;
             }else{
                 start = mid + 1;
             }
         }
-        return end;
+        return -1;
     }
 }
