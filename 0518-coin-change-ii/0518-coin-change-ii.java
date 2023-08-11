5169f389df2820a4ed33fce5dafcbe1043607be6
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-08-11 11:31:09 +0530

    Time: 4 ms (76.11%), Space: 45.9 MB (39.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,25 +1,29 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length; 
-        int[] prev = new int[amount + 1];
-        int[] curr = new int[amount + 1];
-        
-        for(int i = 0; i <= amount; i++){
-            prev[i] = i % coins[0] == 0 ? 1 : 0;
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
         
-        for(int index = 1; index < n; index++){
-            for(int amt = 0; amt <= amount; amt++){
-                int notTake = prev[amt];
-                int take = 0;
-                if(coins[index] <= amt){
-                    take = curr[amt - coins[index]];
-                }
-
-                curr[amt] = take + notTake;
-            }
-            prev = (int[])curr.clone();
+        return findChange(coins.length - 1, coins, amount, dp);
+    }
+    
+    public int findChange(int index, int[] coins, int amount, int[][] dp){
+        if(index == 0){
+            return amount % coins[0] == 0 ? 1 : 0;
         }
-        return prev[amount];
+        
+        if(dp[index][amount] != -1){
+            return dp[index][amount];
+        }
+        
+        int take = 0;
+        if(coins[index] <= amount){
+            take = findChange(index, coins, amount - coins[index], dp);
+        }
+        int notTake = findChange(index - 1, coins, amount, dp);
+        
+        return dp[index][amount] = take + notTake;
     }
 }
