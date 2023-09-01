010519e5022fa20b037544f64c41264179c3410b
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2023-09-01 12:12:41 +0530

    Time: 5 ms (28.82%), Space: 46.1 MB (88.58%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i <= n; i++){
             int count = 0;
             int num = i;
             while(num > 0){
